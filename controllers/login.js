99d29e2557b50d47f541b5d5b98da87771dbf965
1x55 <k4i@pm.me>
2023-07-17 11:35:19 +0900

    updated menu.js to handle deletes

--- controllers/login.js
+++ controllers/login.js
@@ -43,6 +43,7 @@ exports.postLogin = async (req, res) => {
     } else {
         res.render('login', {title: 'Login', loggedIn: false, error:null})
     }
+}
 
     exports.postSignup = async (req, res) => {
         let email = req.body.emailInput
@@ -52,7 +53,7 @@ exports.postLogin = async (req, res) => {
             let users= schemas.users
             let qry = {email:email}
 
-            let userSearch = . await users.findOne(qry)
+            let userSearch = await users.findOne(qry)
             .then(async(data) => {
                  if(!data) {
                     let saltRounds = 10
@@ -60,8 +61,14 @@ exports.postLogin = async (req, res) => {
                         let passHash = await bcrypt.hash(pass, salt, async(err, hash) => {
                             let acct = {email:email, pwd: hash, level:'admin'}
                             let newUser = new schemas.users(acct)
-                            let saveUser = await newUser.save()                        })
+                            let saveUser = await newUser.save()                        
+                        })
                     })
                  }
             })
+            res.render('login', {title:'Login', loggedIn:false, error:'Please login with your new account'});
+
+         } else {
+            res.render('new-acct', {title:'New Account', loggedIn:false, error:'All fields are required. Please check and try again. '});
+            }
 }
--- controllers/menu.js
+++ controllers/menu.js
@@ -1,1 +1,29 @@
 const schemas = require('../models/schemas.js')
+
+module.exports = {
+    getIndex: function (req, res) {
+       res.render ('index', {title: 'Menu Items'})
+    },
+    editMenu: async(req,res) => {
+        let sesh = req.session
+  
+        if (!sesh.loggIn) { 
+            res.render ('menu', {title: 'Edit', loggedIn: false, error: "Invalid Request"})
+        } else {
+            let id = req.params.id
+            let err = ''
+
+            let menu = schemas.menu
+            let qry= {_id:id}
+
+            let itermResult = await menu.find(qry)
+            .then((itemData) => {
+                if (itemData == null) {
+                    err = 'Invalid ID'
+                }
+
+                res.render ('menu', {title: 'Edit Menu', loggedIn: sesh.loggedIn , error: err})
+            })
+        }
+    }
+}
