6db0c6084a12b794a76e0cb37d61a53cc15a94a3
1x55 <k4i@pm.me>
2023-07-17 10:40:30 +0900

    updated login.js controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,1 +1,46 @@
-const schemas = require('../models/schemas.js')
+const schemas = require('../models/schemas.js') 
+const bcrypt = require('bcrypt')
+
+exports.getLogin = (req, res) => {
+    res.render('login', {title: 'Login', loggedIn: false, error:null})
+}
+
+exports.getSignup = (req, res) => {
+    res.render('new-acct', {title: 'New Account',  loggedIn: false, error:null})
+}
+
+exports.getLogout = (req, res) => {
+    res.session.destroy()
+    res.redirect('/')
+}
+
+exports.postLogin = async (req, res) => {
+    let email = req.body.emailInput
+    let pass = req.bodypwdInput
+    let loginSuccess = false
+    let sesh = req.sessiom
+    sesh.loggIn = false
+
+    let users = schemas.users
+    let qry = {email:email}
+
+    if(email !== '' && pass !== '' ){
+        let userResult = await users.findOne(qry)
+        .then(async(data) => {
+            if (data) {
+                let passResult = await bcrypt.copmpare(pass, data.pwd)
+                .then((isMatch) => {
+                    if (isMatch) => {
+                    sesh.loggedIn = true
+                    loginSuccess = true
+                    }
+                })
+            }
+        })
+    }
+    if (loginSuccess === true) {
+        res.redirect('/')
+    } else {
+        res.render('login', {title: 'Login', loggedIn: false, error:null})
+    }
+}
